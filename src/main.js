2830afd254e2517c8b94f4831996415abf818cff
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-01-15 15:52:05 -0300

    fix Authorization

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import './config/msgs'
 
 Vue.config.productionTip = false
 
-// TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkdhYnJpZWwiLCJlbWFpbCI6ImdhYnJpZWxuYXBvbGlzMTYwNEBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzA1MzQyMjI1LCJleHAiOjE3MDU2MDE0MjV9.6RLPgkmHfWg9i_Tn7dwqwacWd5m_Fs4QfZzrWTELwfA'
 
 new Vue({
   store,
